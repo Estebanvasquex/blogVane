e4c207bc39053effa4821878882e4a8f722ca91e
Estebanvasquex <53843278+Estebanvasquex@users.noreply.github.com>
2024-06-16 23:10:45 -0500

    se crean las tarjetas con su respectivo ejemplo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,78 @@
-import Image from "next/image";
+
+
+
+import CardBlog from '@/components/commons/card-blog/CardBlog';
+
+import { link } from 'fs';
 
 export default function Home() {
+
+  const dataCards = [
+    {
+      title: 'Hasta el minimo detalle',
+      description: 'Dios conoce tu necesidad y está pendiente de cada detalle de tu vida.',
+      date: '2021-09-01',
+      image: 'https://images.unsplash.com/photo-1518674660708-0e2c0473e68e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      link: '',
+    },
+    {
+      title: 'La estrategia del león',
+      description: 'Aunque no es el felino ni más velos ni más fuerte, si es el mas intelingente y el mas cooperativo, por eso se le conoce como el rey de la selva.',
+      date: '2021-09-01',
+      image: 'https://images.unsplash.com/photo-1512614521719-2806f69d406b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      link: 'https://images.unsplash.com/photo-1630676938982-0f2b8d6b2b6b',
+    },
+    {
+      title: '¿De que estas lleno?',
+      description: 'Cuando el globo está solo con aire no soporta el calor del fuego pero cuando está lleno de agua esta absorbe el calor y no le permite explotar.',
+      date: '2021-09-01',
+      image: 'https://images.unsplash.com/photo-1602153883150-a442f874f839?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      link: 'https://images.unsplash.com/photo-1630676938982-0f2b8d6b2b6b',
+    },
+    {
+      title: 'Cual es tu filtro',
+      description: 'La pupila o niña de los ojos es la que determina la cantidad de luz que entra al ojo es por ello que al estar espuesta a más cantidad de luz esta se pone pequeña y cuado hay escasez de luz esta se pone grande.',
+      date: '2021-09-01',
+      image: 'https://images.unsplash.com/photo-1494869042583-f6c911f04b4c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      link: 'https://images.unsplash.com/photo-1630676938982-0f2b8d6b2b6b',
+    },
+    {
+      title: 'Cuando el deber te llama, atiendele',
+      description: 'Saber que debemos hacer algo y no hacerlo puede cambiar nuestro destino, por eso no postergues tu deber.',
+      date: '2021-09-01',
+      image: 'https://images.unsplash.com/photo-1620058623533-1f9a64e9baad?q=80&w=1801&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      link: 'https://images.unsplash.com/photo-1630676938982-0f2b8d6b2b6b',
+    },
+  ]
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="py-16 bg-gradient-to-br from-green-50 to-cyan-100">  
+    <h1 className="text-lg font-medium">esto es una prueba</h1>
+    <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+      <div className="mb-12 space-y-2 text-center">
+        <span className="block w-max mx-auto px-3 py-1.5 border border-green-200 rounded-full bg-green-100 text-green-600">Blog</span>
+        <h2 className="text-2xl text-cyan-900 font-bold md:text-4xl">Sin mi nada podreís</h2>
+        <p className="lg:w-6/12 lg:mx-auto">Palabras de vida para sanar y alimentar el alma</p>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="grid gap-12 lg:grid-cols-2">
+      {
+          dataCards.map((card, index) => (
+            <CardBlog key={index} title={card.title} description={card.description} date={card.date} image={card.image} link={card.link}/>
+          ))
+        }
+
+
+
       </div>
+    </div>
+  </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
+
+
+
+
---
+++ src/components/commons/card-blog/CardBlog.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface CardBlogProps {
+  title: string
+  description: string
+  link: string
+  image: string
+  date: string
+}
+
+const CardBlog = (props: CardBlogProps ) => {
+  const { title, description, link, image, date } = props
+/*
+  src="/profile.png"
+  width={243}
+  height={216}
+  alt="Picture of the author"
+  */
+
+
+
+  return (
+<div className="p-1 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
+          <Image src={image} alt="art cover" loading="lazy" width={243} height={216} className="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl" />
+          <div className="sm:w-7/12 pl-0 p-5">
+            <div className="space-y-2">
+              <div className="space-y-4">
+                <h4 className="text-2xl font-semibold text-cyan-900">{title}</h4>
+                <p className="text-gray-600">{description}</p>
+              </div>
+              <a href={link} className="block w-max text-cyan-600">Read more</a>
+            </div>
+          </div>
+        </div>
+  )
+}
+
+export default CardBlog
+
