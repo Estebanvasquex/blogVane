52af8977ca165ab6f4f1a077a5cec97654c2bafb
Estebanvasquex <53843278+Estebanvasquex@users.noreply.github.com>
2024-06-16 23:16:07 -0500

    Se borra h1 que no aporta al contenido

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,6 @@ export default function Home() {
 
   return (
     <div className="py-16 bg-gradient-to-br from-green-50 to-cyan-100">  
-    <h1 className="text-lg font-medium">esto es una prueba</h1>
     <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
       <div className="mb-12 space-y-2 text-center">
         <span className="block w-max mx-auto px-3 py-1.5 border border-green-200 rounded-full bg-green-100 text-green-600">Blog</span>
